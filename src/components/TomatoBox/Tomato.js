bc899ebbc5db564d826a3eca413987b6164dde06
sudimac <vojvoda1988@gmail.com>
2022-07-13 22:37:53 +0200

    prvi commit

---
+++ src/components/TomatoBox/Tomato.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./tomato.css";
+
+const Tomato = () => {
+  return <div className="tomato">0</div>;
+};
+
+export default Tomato;
---
+++ src/components/blueBox/blue.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./blue.css";
+
+const Blue = () => {
+  return <div className="blue">0</div>;
+};
+
+export default Blue;
